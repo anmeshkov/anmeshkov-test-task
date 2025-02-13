0cd45896e397fdf15943527e72d65d96f7fd0c1e
Andrew Meshkov <meshkov89@gmail.com>
2025-02-13 12:51:35 +0300

    feat: выполнена верска мобильного меню

--- src/index.js
+++ src/index.js
@@ -2,3 +2,22 @@ import './styles/scss/main.scss';
 import './index.html';
 
 console.log('anmeshkov test task');
+
+document.querySelector('#mobile-nav-button').addEventListener('click', () => {
+  document.querySelector('#mobile-nav-menu').classList.toggle('hide');
+  document.querySelector('body').classList.toggle('no-scroll');
+});
+
+document.querySelectorAll('.mobile-nav__item').forEach((items) => {
+  items.addEventListener('click', () => {
+    document.querySelector('#mobile-nav-menu').classList.toggle('hide');
+    document.querySelector('body').classList.toggle('no-scroll');
+  });
+});
+
+document.querySelector('#mobile-nav-menu').addEventListener('click', (e) => {
+  if (!e.target.classList.contains('mobile-nav__item')) {
+    document.querySelector('#mobile-nav-menu').classList.add('hide');
+    document.querySelector('body').classList.remove('no-scroll');
+  }
+});
